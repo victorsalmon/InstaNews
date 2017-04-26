2a8b53a9893bad8ca87561708814275ad689c010
Victor Salmon <vasalmon@hotmail.com>
2017-04-26 11:27:05 -0700

    Finished script to uglify and rename js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var gulp = require('gulp'),
     rename = require('gulp-rename');
 
 gulp.task('default', function(){
-  gulp.src ('./js/*.js').pipe()
+  gulp.src ('./js/*.js')
+    .pipe(uglify ())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('./build/js'));
   gulp.dest('test.min.js');
 });
