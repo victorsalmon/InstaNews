df211575d3ba2dc8a2013985e282eff88add168c
Victor Salmon <vasalmon@hotmail.com>
2017-04-27 18:14:49 -0700

    Completed API request portion of js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,35 @@
 
 $(function (){
-  
+  //Initialize jQuery-Nice-Select
+  $('select').niceSelect();
+
+  //Wait for selection and then AJAX
   $('select').on('change', function(){
     var menuChoice = $(this).val();
     var url = 'https://api.nytimes.com/svc/topstories/v2/' + menuChoice + '.json?'
     + $.param({
       'api-key': '385dbba7ced942639ee354d262342235',
-      'multimedia':'true',
     }, {passive:true} );
-    console.log ('url: ' + url);
 
+    //Retrieve NYT Content
     $.ajax({
       url: url,
       method: 'GET',
-    }).done(function(result) {  //Successfully get articles and output image and abstract
-      result.results.splice(12)
-      
-      
+    }).always(function (){
+      $('.loading').append('<img src="../images/ajax-loader.gif">');
+    }).done(function(result) {
+    
+      //Process Results Object
+      var resultsArray = result.results.filter (function(object){
+        return object.multimedia.length;
+      });
+      resultsArray.splice(12);
+ //     $(.grid).append(function(){
+ //       each (resultsArray)
+ //     })
+      //Output Content to Page
+      console.log (resultsArray);
+
     }).fail(function(err) {
       throw err;
     });
